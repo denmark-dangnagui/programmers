144e5e67150a4dcfdadbfd8c9549be7a4da48381
denmark-dangangui <dlrgks88@naver.com>
2022-08-19 00:08:38 +0900

    update

--- Stack&Queue/기능개발.py
+++ Stack&Queue/기능개발.py
@@ -1,26 +1,25 @@
 from collections import deque
-
-progresses = [95, 90, 99, 99, 80, 99]
-speeds = [1, 1, 1, 1, 1, 1]
-p = deque(progresses)
-s = deque(speeds)
-result = []
-qwe = 0
-while len(p) != 0:
-    qwe += 1
-    cnt = 0
-    for i in range(len(p)):
-        if p[i] == 100:
-            continue
-        else:
-            p[i] += s[i]
-    while p:
-        if p[0] == 100:
-            cnt += 1
-            p.popleft()
-            s.popleft()
-        else:
-            break
-    result.append(cnt)
-
-print(result)
+def solution(progresses, speeds):
+    p = deque(progresses)
+    s = deque(speeds)
+    result = []
+    r = []
+    while len(p) != 0:
+        cnt = 0
+        for i in range(len(p)):
+            if p[i] >= 100:
+                continue
+            else:
+                p[i] += s[i]
+        while p:
+            if p[0] >= 100:
+                cnt += 1
+                p.popleft()
+                s.popleft()
+            else:
+                break
+        result.append(cnt)
+    for i in result:
+        if i != 0:
+            r.append(i)
+    return r
