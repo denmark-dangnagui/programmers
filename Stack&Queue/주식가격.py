b54d97f562ccf427500d23c3f9c756de426f7c55
denmark-dangangui <dlrgks88@naver.com>
2022-08-19 15:27:32 +0900

    Update 주식가격.py

--- Stack&Queue/주식가격.py
+++ Stack&Queue/주식가격.py
@@ -2,14 +2,12 @@ from collections import deque
 def solution(prices):
     answer = []
     p = deque(prices)
-    t = len(p)
     while p:
         sec = 0
-        print(p)
         for j in range(len(p)-1):
-            print(1)
-            if p[0] <= p[j+1]:
-                sec += 1
+            sec += 1
+            if p[0] > p[j+1]:
+                break
         p.popleft()
         answer.append(sec)
     return answer
